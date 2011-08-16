9e5c1c19a6f5e086d6333b32debe5ce64b542a8f
Eric Pearson <epearson@rim.com>
2011-08-16 12:39:38 -0400

    Pass more testcases for eventbus

--- Starbuck/starbuck.cpp
+++ Starbuck/starbuck.cpp
@@ -113,13 +113,13 @@ void Starbuck::registerInternalAPIs()
 {
     QWebFrame* frame = webViewInternal->page()->mainFrame();
     frame->addToJavaScriptWindowObject(QString("eventbus2"), new BlackBerryBus(this, frame));
-//    frame->evaluateJavaScript("eventbus2.initbus()");
+    frame->evaluateJavaScript("eventbus2.initbus()");
 
     // check for iframes, if found add to window object
     for(int i = 0; i < frame->childFrames().length(); i++)
     {
         frame->childFrames()[i]->addToJavaScriptWindowObject(QString("eventbus2"), new BlackBerryBus(this, frame->childFrames()[i]));
-//        frame->childFrames()[i]->evaluateJavaScript("eventbus2.initbus()");
+        frame->childFrames()[i]->evaluateJavaScript("eventbus2.initbus()");
     }
 }
 
